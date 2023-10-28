4852caf4bc82390876e45f181a04d9e3da7d8fb1
Victor Higaki <victorhigaki@outlook.com>
2023-10-28 04:19:59 -0300

    feat: create User Service and Controller

---
+++ src/main/java/com/desafiobackend/dtos/UserDTO.java
@@ -0,0 +1,8 @@
+package com.desafiobackend.dtos;
+
+import com.desafiobackend.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
